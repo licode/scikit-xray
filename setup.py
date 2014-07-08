8fea036a42cd60edb98d1b01949f8acb23717ceb
Eric Dill <edill@bnl.gov>
2014-07-08 10:55:31 -0400

    MNT: Updated setup.py to reflect version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='NSLS2',
-    version='0',
+    version='0.0.1',
     author='Brookhaven National Lab',
     packages=["nsls2", "nsls2.io"],
     )
